86f2ea46acf74c90850fcca7a4611a10448df980
Richard J <richjim89@gmail.com>
2018-10-16 18:02:46 -0700

    first child class

---
+++ Zoo/Zoo/Classes/Vertebrate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    //Inherited from Animal, base class to more abstract classes
+    public abstract class Vertebrate : Animal
+    {
+        //Creating new abstract property
+        public abstract string Blood { get; set; }
+
+        //Creating a virtual method for optional overriding
+        public virtual string Sound()
+        {
+            return null;
+        }
+
+        //Overriding parent abstract property
+        public override bool Spine { get; set; } = true;
+    }
+}
