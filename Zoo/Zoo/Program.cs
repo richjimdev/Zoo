8824c73b3a76de185465e22c2bc0c80359172ba3
Richard J <richjim89@gmail.com>
2018-10-16 18:40:06 -0700

    Instantiated some animal from concrete classes

--- Zoo/Zoo/Program.cs
+++ Zoo/Zoo/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using Zoo.Classes;
 
 namespace Zoo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PopulateZoo();
+        }
+
+        public static void PopulateZoo()
+        {
+            Lion mufasa = new Lion();
+            Lion simba = new Lion();
+            Eagle baldie = new Eagle();
+            Ostrich matilda = new Ostrich();
+
+
+            Console.WriteLine("A lion says: " + mufasa.Sound());
+            Console.WriteLine("An eagle says: " + baldie.Sound());
+            Console.WriteLine($"An ostrich can lay an average of {matilda.AverageEggsLaid()} eggs.");
         }
     }
 }
