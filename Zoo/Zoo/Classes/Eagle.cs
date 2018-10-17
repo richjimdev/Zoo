366b667ea511f848011ac39219b167cfc99f865f
Richard J <richjim89@gmail.com>
2018-10-16 18:30:24 -0700

    created eagle class

---
+++ Zoo/Zoo/Classes/Eagle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    /// <summary>
+    /// Concrete class derrived from Bird
+    /// </summary>
+    public class Eagle : Bird
+    {
+        //Overriding both abstract methods passed from parent classes
+        public override int AverageEggsLaid()
+        {
+            return 5 + 2;
+        }
+
+        public override string Nutrition()
+        {
+            return "rodents";
+        }
+    }
+}
