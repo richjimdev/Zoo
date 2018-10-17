85a984269a483afa952d7f8a9d9657130ce3c0c2
Richard J <richjim89@gmail.com>
2018-10-16 18:28:14 -0700

    create Lion class

---
+++ Zoo/Zoo/Classes/Lion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    /// <summary>
+    /// Concrete class derrived from Mammal
+    /// </summary>
+    public class Lion : Mammal
+    {
+        //overriding abstract method from 3 levels up
+        public override string Nutrition()
+        {
+            return "meat";
+        }
+
+        //overriding virtual method from parent
+        public override string Sound()
+        {
+            return "Roar!";
+        }
+    }
+}
--- Zoo/Zoo/Classes/Mammal.cs
+++ Zoo/Zoo/Classes/Mammal.cs
@@ -4,13 +4,16 @@ using System.Text;
 
 namespace Zoo.Classes
 {
+    /// <summary>
+    /// Abstract class derrived from Vertebrate
+    /// </summary>
     public abstract class Mammal : Vertebrate
     {
         //overriding abstract property
         public override string Blood { get; set; } = "warm";
 
         //new virtual method for optional override
-        public virtual bool laysEggs { get; set; } = false;
+        public virtual bool LaysEggs { get; set; } = false;
 
     }
 }
