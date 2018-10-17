4b19f9680a1c9abe15701b4dc1e8c0d7e69e382b
Richard J <richjim89@gmail.com>
2018-10-16 18:11:51 -0700

    created first interface

---
+++ Zoo/Zoo/Interfaces/ICanFly.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Interfaces
+{
+    interface ICanFly
+    {
+        bool wings { get; set; }
+
+        string Fly();
+    }
+}
