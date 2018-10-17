82575a1c946211079527e8948c1616e55a28f3d2
Richard J <richjim89@gmail.com>
2018-10-16 19:02:44 -0700

    crab

---
+++ Zoo/Zoo/Classes/Crab.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    /// <summary>
+    /// Concrete class derrived from Crustacean
+    /// </summary>
+    public class Crab : Crustacean
+    {
+        //overriding method from parent class
+        public override string Nutrition()
+        {
+            return "Algae";
+        }
+    }
+}
