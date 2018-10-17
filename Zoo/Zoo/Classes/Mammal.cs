1667beff5c013c7459e104c5568942f9d4eba257
Richard J <richjim89@gmail.com>
2018-10-16 18:24:20 -0700

    create mammal class

---
+++ Zoo/Zoo/Classes/Mammal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    public abstract class Mammal : Vertebrate
+    {
+        //overriding abstract property
+        public override string Blood { get; set; } = "warm";
+
+        //new virtual method for optional override
+        public virtual bool laysEggs { get; set; } = false;
+
+    }
+}
