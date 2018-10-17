3b6a1540279f39970ce118d9dbbda9cf16214945
Richard J <richjim89@gmail.com>
2018-10-16 17:58:24 -0700

    Created first base class

---
+++ Zoo/Zoo/Classes/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    /// <summary>
+    /// The main Base class for all my classes.
+    /// </summary>
+    public abstract class Animal
+    {
+        //Regular Property
+        public string Domain { get; set; } = "Eukaryota";
+
+        //Abstract method
+        public abstract string Nutrition();
+
+        //Abstract property
+        public abstract bool Spine { get; set; }
+    }
+    
+}
